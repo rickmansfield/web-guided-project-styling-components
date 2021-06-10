ae35ce0742db0dbb998ebcf2803d85bcafe24dc9
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-06-10 14:02:44 -0400

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,5 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const kf = keyframes`
+  50% {
+    transform: scale(0.8)
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1)
+  }
+`
 
 const StyledFriend = styled.div`
   width: 60%;
@@ -10,26 +20,31 @@ const StyledFriend = styled.div`
 
   background-color: ${props => props.theme.primaryColor};
   color: ${({ theme }) => theme.white};
-
+  
   @media ${({ theme }) => theme.breakpointMobile} {
     width: initial;
   }
-
+  
   transition: all 0.2s ease-in-out;
   &:hover {
     transition: all 0.2s ease-in-out;
     background-color: ${({ theme }) => theme.secondaryColor};
   }
-
+  
   &::before {
     content: "${props => props.besty ? '💚' : '🙂'}"
   }
 
-  button {
-    background-color: ${props => props.theme.tertiaryColor};
-    &:hover {
-      transform: scale(5);
-    }
+  transform: scale(2);
+  opacity: 0;
+  animation: ${kf} 0.3s ease-in-out forwards;
+
+  
+`
+const StyledButton = styled.button`
+  background-color: ${props => props.theme.tertiaryColor};
+  &:hover {
+    transform: scale(1.1);
   }
 `
 
@@ -37,9 +52,9 @@ export default function Friend({ info, action, besty }) {
   return (
     <StyledFriend besty={besty}>
       {info.name}
-      <button onClick={() => action(info.id)}>
+      <StyledButton onClick={() => action(info.id)}>
         See details
-      </button>
+      </StyledButton>
     </StyledFriend>
   )
 }
