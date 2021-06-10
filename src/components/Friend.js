8029d5cb96e31d2f25208d52c8c4880d46d7c803
Christopher Atoki <christopheratoki@Christophers-MacBook-Pro.local>
2021-06-10 13:00:12 -0400

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFriend = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  border-bottom: 2px solid white;
+
+  background-color: ${({ theme }) => theme.primaryColor};
+  color: white;
+`
 
 export default function Friend({ info, action }) {
   return (
-    <div className='friend'>
+    <StyledFriend>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
